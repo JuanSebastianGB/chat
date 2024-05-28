8217a4fe195afb84230a668cd7324ad08fca34e4
Juanc <3393@holbertonstudents.com>
2024-05-27 20:50:21 -0500

    feat: :memo: Added cunsymer and rounting

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -2,6 +2,10 @@ import os
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
+from channels.routing import URLRouter
+from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
@@ -9,6 +13,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        # Just HTTP for now. (We can add other protocols later.)
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
     }
 )
