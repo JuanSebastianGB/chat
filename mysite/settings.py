cf8cd7e49c4e8afe0add218c59bdd4be2ebdd362
Juanc <3393@holbertonstudents.com>
2024-05-27 20:38:26 -0500

    feat: :fire: Installed daphne

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
+    "chat",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "chat",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Daphne
+ASGI_APPLICATION = "mysite.asgi.application"
